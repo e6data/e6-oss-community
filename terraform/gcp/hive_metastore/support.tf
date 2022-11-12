5bbcfb09179e011c47b58af748dd9c31bc65b93f
Harshith <104987110+Harshithraj24@users.noreply.github.com>
2022-11-12 10:42:43 +0530

    <harshith>firewall

--- terraform/gcp/hive_metastore/support.tf
+++ terraform/gcp/hive_metastore/support.tf
@@ -10,3 +10,17 @@ data "google_compute_network" "metastore_network" {
 data "google_compute_zones" "zones" {
   region = var.region
 }
+
+/// This resource is needed to access dataproc via cli
+
+# resource "google_compute_firewall" "metastore_firewall" {
+#   name          = "hive-metastore-firewall"
+#   network       = data.google_compute_network.metastore_network.self_link
+#   source_tags   = ["metastore"]
+#   source_ranges = ["0.0.0.0/0"]
+#   target_tags   = ["metastore"]
+#   allow {
+#     protocol = "TCP"
+#     ports    = [var.metastore_port]
+#   }
+# }
